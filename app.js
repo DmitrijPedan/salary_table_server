cc9207a4f12773c5f0a68f08208aa41fff0cacff
DmitrijPedan <dmitrijpedan84@gmail.com>
2020-02-14 09:13:57 +0200

    init commit

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const stuff = require('./stuff');
+
+app.get('/api', (req, res) => {
+
+    const perPage = 'perPage' in req.query && Number(req.query.perPage) ? Number(req.query.perPage) : 3;
+    const currentPage = 'page' in req.query && Number(req.query.page)? Number(req.query.page) : 1;
+    const urlPrefix = '/api?page=';
+    const pages = Math.ceil(stuff.length/perPage);
+
+    res.json({
+        info: {
+            count: stuff.length,
+            pages,
+            next: getNextPageUrl(currentPage, pages, urlPrefix),
+            prev: getPrevPageUrl(currentPage, urlPrefix),
+            currentPage,
+            perPage,
+          },
+          results: stuff.slice((currentPage-1)*perPage,currentPage*perPage)
+    })
+});
+
+const getNextPageUrl = (current_page, total_pages, url_prefix) => {
+    return current_page < total_pages ? `${url_prefix}${current_page+1}` : null
+}
+const getPrevPageUrl = (current_page, url_prefix) => current_page > 1 ? `${url_prefix}${current_page-1}` : null
+
+app.listen(3000, () => {
+    console.log('Server was runnig...')
+})
