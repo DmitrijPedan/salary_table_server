8242fb293c971dffdd0fb02e5a760bca569338c1
DmitrijPedan <dmitrijpedan84@gmail.com>
2020-03-16 12:25:47 +0200

    add ws server

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
 const express = require('express');
 const app = express();
 const stuff = require('./stuff');
-const PORT = process.env.PORT || 3000;
-app.get('/api', (req, res) => {
+const stocks = require('./stocks');
 
+const HTTP_PORT = process.env.PORT || 3000;
+const WS_PORT = process.env.PORT || 8181;
+
+const WebSocketServer = require('ws').Server;
+const wsServer = new WebSocketServer({port: WS_PORT});
+
+wsServer.on('connection', ws => {
+    console.log('WS connection was detected');
+    const generateRandomFloat = (min, max) => Number((Math.random()*(max-min) + min).toFixed(2))
+
+    const updateStockRandomly = () => {
+        Object.keys(stocks).map(key => stocks[key] += generateRandomFloat(-100,100));
+        ws.send(JSON.stringify(stocks))
+        setTimeout(updateStockRandomly, generateRandomFloat(10,90)*100)
+    }
+    updateStockRandomly();
+})
+
+
+app.get('/api', (req, res) => {
     const perPage = 'perPage' in req.query && Number(req.query.perPage) ? Number(req.query.perPage) : 3;
     const currentPage = 'page' in req.query && Number(req.query.page)? Number(req.query.page) : 1;
     const urlPrefix = '/api?page=';
@@ -22,11 +41,15 @@ app.get('/api', (req, res) => {
     })
 });
 
+app.get('/stocks', (req,res) => {
+    res.json(stocks);
+})
+
 const getNextPageUrl = (current_page, total_pages, url_prefix) => {
     return current_page < total_pages ? `${url_prefix}${current_page+1}` : null
 }
 const getPrevPageUrl = (current_page, url_prefix) => current_page > 1 ? `${url_prefix}${current_page-1}` : null
 
-app.listen(PORT, () => {
+app.listen(HTTP_PORT, () => {
     console.log('Server was runnig...')
 })
---
+++ stocks.js
@@ -0,0 +1,6 @@
+module.exports = {
+    GOOG: 550.23,
+    AAPL: 100.52,
+    MSFT: 50.36,
+    AMZN: 470.25
+}
